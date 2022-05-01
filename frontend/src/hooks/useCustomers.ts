8e49967dbaa9a087f1798f606a491fbe1209035e
Imad Machi <machii.imad@gmail.com>
2022-05-01 18:13:56 +0000

    add useMemo hook

--- frontend/src/hooks/useCustomers.ts
+++ frontend/src/hooks/useCustomers.ts
@@ -1,15 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { fetchCutomers, selectCustomers } from "../features/slices/customerSlice";
 import { useAppDispatch } from "../features/store";
 import { useTypedSelector } from "./useTypedSelector";
 
 export const useCustomers = () => {
-  const { loading, error, customers } = useTypedSelector(selectCustomers);
+  const data = useTypedSelector(selectCustomers);
   const dispatch = useAppDispatch();
   const load = () => dispatch(fetchCutomers(null));
   useEffect(() => {
     load();
   }, []);
 
-  return { loading, error, customers };
+  return useMemo(() => data, [data]);
 };
